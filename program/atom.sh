9eca32155317531377442b8cb959f56b614148e2
Emeric MARTINEAU <11473190+bubulemaster@users.noreply.github.com>
2018-07-04 13:53:15 +0200

    Remove twice dependencies for atom

--- program/atom.sh
+++ program/atom.sh
@@ -5,7 +5,7 @@ local ATOM_VERSION=v1.27.2
 APPLICATION_DOWNLOADED_FILE_NAME='atom.deb'
 APPLICATION_URL="https://github.com/atom/atom/releases/download/${ATOM_VERSION}/atom-amd64.deb"
 APPLICATION_IMAGE_DOCKER="run-atom:${ATOM_VERSION}"
-APPLICATION_DEPENDENCIES="ca-certificates curl fakeroot gconf2 gconf-service git gvfs-bin libasound2 libcap2 libgconf-2-4 libgtk2.0-0 libnotify4 libnss3 libxkbfile1 libxss1 libxtst6 libgl1-mesa-glx libgl1-mesa-dri python xdg-utils libcanberra-gtk3-module libgtk2.0-0 libudev1 libx11-xcb1 libsecret-1-0 gir1.2-gnomekeyring-1.0"
+APPLICATION_DEPENDENCIES="ca-certificates curl fakeroot gconf2 gconf-service git gvfs-bin libasound2 libcap2 libgconf-2-4 libgtk2.0-0 libnotify4 libnss3 libxkbfile1 libxss1 libxtst6 libgl1-mesa-glx libgl1-mesa-dri python xdg-utils libcanberra-gtk3-module libudev1 libx11-xcb1 libsecret-1-0 gir1.2-gnomekeyring-1.0"
 
 # -f : tell atom to not release bash session
 APPLICATION_COMMAND_LINE="/usr/bin/atom -f"
